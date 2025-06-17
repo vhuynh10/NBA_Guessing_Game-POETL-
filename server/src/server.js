266a94bc3363c3df8854cb38723da3054d0a2cc9
Vinny Huynh <huynhvinny100@gmail.com>
2025-06-16 22:27:31 -0700

    nevermind the dash was needed

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://nba-guessing-game-poetl-beta.vercel.app'],
+    origin: ['http://localhost:5173', 'https://nba-guessing-game-poetl-beta.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
