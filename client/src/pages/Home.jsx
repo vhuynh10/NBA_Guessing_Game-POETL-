59642c6fb3e2ee09f9300747845daf0ecc5241a8
Vinny Huynh <huynhvinny100@gmail.com>
2025-06-16 21:43:14 -0700

    Adjusted names of the URL

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   const [error, setError] = useState(null); 
   const Navigate = useNavigate();
 
-  const API_URL = process.env.REACT_APP_API_URL
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const goToMainGame = () => {
     Navigate("/game"); // Redirect to the MainGame page
